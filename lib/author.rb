7bb64287411f258f6423c8d2be63863af3fde5ea
Anaxirinia Jimenez <anax131@gmail.com>
2018-01-02 04:15:30 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name, :posts, :title
+  @@post_count = 0
 
   def initialize(name)
     @name = name
@@ -19,7 +20,10 @@ class Author
   def add_post_by_title(post_title)
     post = Post.new(post_title)
     @posts << post
+  end
 
+  def self.post_count
+    @@post_count
   end
 
 end
