6421f6f8af89f51faff1315822259caceecb644f
Anaxirinia Jimenez <anax131@gmail.com>
2018-01-02 04:36:12 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,8 @@
 class Post
-  attr_accessor :name, :author
+  attr_accessor :title, :author
 
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title =  title
   end
 
   def title
