0bef05c373cef54f051de18374f96909b227a53d
Anaxirinia Jimenez <anax131@gmail.com>
2018-01-02 03:40:06 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+  attr_accessor :name
+  @@song = nil
+  @@song_count = 0
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+
+  end
+
+  def song_count
+    @songs.length
+  end
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,9 @@
+class Post
+  attr_accessor :name, :author
+
+  def initialize(name)
+    @name = name
+  end
+
+  
+end
