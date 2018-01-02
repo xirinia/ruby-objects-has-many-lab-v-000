3e30a6b5583a3aa64914137f08776a9bfc53dfee
Anaxirinia Jimenez <anax131@gmail.com>
2018-01-02 04:00:43 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,10 +15,10 @@ class Author
     post.author = self
   end
 
-  def add_post_by_title(post)
-    post = Post.new(post)
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
     @posts << post
-    post.author = self
+    
   end
 
 end
